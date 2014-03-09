23716752b55a995c6b0ad1b5b89c4582d0d5fb96
Sheehan <421036289@qq.com>
2014-03-10 01:52:07 +0800

    add some functions

--- LList.py
+++ LList.py
@@ -32,6 +32,31 @@ class LList(object):
             node = node.link
         return node
 
+        def _delete(self, position):
+            # private method to delete item at location position from the list
+            # pre: 0 <= position < self.size
+            # post: the item at the specified position is removed from teh list
+            #       and the item is returned (for use with pop)
+
+            if position == 0:
+                # save item from the initial node
+                item = self.head.item
+
+                # change self.head to point "over" the deleted node
+                self.head = self.link
+            else:
+                # find the node immediately before the deleted node
+                prev_node = self._find(position - 1)
+
+                # save the item from node to delete
+                item = prev_node.link.item
+
+                # change predecessor to point "over" the deleted node
+                prev_node.link = prev_node.link.link
+
+            self.size = -1
+            return item
+
     def append(self, x):
 
         """appends x onto end of the list
@@ -50,3 +75,32 @@ class LList(object):
             # set self.head to new node
             self.head = newNode
         self.size += 1
+
+    def __getitem__(self, position):
+
+        """return data item at location position
+        pre: 0 <= position < size
+        post: returns data item at the specified position"""
+
+        node = self._find(position)
+        return node.item
+
+    def __setitem__(self, position, value):
+
+        """set data item at location position to value
+        pre: 0 <= position < self.size
+        post: sets the data item at the specified position to value"""
+
+        node = self._find(position)
+        node.item = value
+
+    def __delitem__(self, position):
+
+        """delete item at location position form the list
+        pre: 0 <= position < self.size
+        post: the item at he specified position is removed from
+        the list"""
+
+        assert 0 <= position < self.size
+
+        self._delete(self, position)
